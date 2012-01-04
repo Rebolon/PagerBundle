fd001d4d59149b3ed18e5d3c0fc2378487e9372b
Benjamin RICHARD <benjamin.richard@infolegale.fr>
2012-01-04 16:09:35 +0100

    Modification on autoloading : use composer/composer project

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,13 +1,17 @@
 <?php
 
-/*
- * This file is part of the RebolonPagerBundle package.
- *
- * It has been freely inspired by FOSUserBundle
- */
+$loader = require __DIR__.'/../vendor/.composer/autoload.php';
+$loader->register();
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
-}
+// @todo : resolve this autoload requirements whereas it should not be !!!
+spl_autoload_register(function($class) {
+	if (0 === strpos($class, 'Rebolon\\PagerBundle\\')) {
+		$path = __DIR__ . '/../' . implode('/', array_slice(explode('\\', $class), 2)) . '.php';
+
+		if (!stream_resolve_include_path($path)) {
+			return false;
+		}
+		require_once $path;
+		return true;
+	}
+});
