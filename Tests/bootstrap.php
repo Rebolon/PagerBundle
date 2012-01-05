12b6923b8b9a9948204bfbba6d5d44254d3bcb3b
Rebolon <richard.tribes@gmail.com>
2012-01-05 12:02:08 +0100

    Change on autoload from composer.json so no more needs specific autoload (supposed that bundle is installed in WhereEverYouWant/Rebolon/PagerBundle)

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,17 +1,3 @@
 <?php
-
 $loader = require __DIR__.'/../vendor/.composer/autoload.php';
 $loader->register();
-
-// @todo : resolve this autoload requirements whereas it should not be !!!
-spl_autoload_register(function($class) {
-	if (0 === strpos($class, 'Rebolon\\PagerBundle\\')) {
-		$path = __DIR__ . '/../' . implode('/', array_slice(explode('\\', $class), 2)) . '.php';
-
-		if (!stream_resolve_include_path($path)) {
-			return false;
-		}
-		require_once $path;
-		return true;
-	}
-});
